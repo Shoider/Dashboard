8301c41554349d597423a7ab41cf18479a560aeb
NidiaVGarcia <nidiavgarcia12@gmail.com>
2025-04-29 16:37:52 -0600

    fix(imagenes):se borro lo que no se ocupa

--- src/sections/auth/sign-in-view.tsx
+++ src/sections/auth/sign-in-view.tsx
@@ -1,9 +1,9 @@
 import { useState, useCallback } from 'react';
 
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+//import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
+//import Divider from '@mui/material/Divider';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
