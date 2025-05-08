8195622f1b7dccc9abd5ae8bbfad9a9bedebbc41
BrandonHS <branditis1211@gmail.com>
2025-05-08 16:26:57 -0600

    feat(Token): Validar Token

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import type { ReactNode } from 'react';
+
+import axios from 'axios';
+import { useState, useEffect, useContext, createContext } from 'react';
 
 type AuthContextType = {
   isAuthenticated: boolean;
@@ -9,7 +12,29 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(() => !!localStorage.getItem("authToken"));
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // Función para verificar la validez del token
+  const verifyToken = async () => {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      setIsAuthenticated(false);
+      return;
+    }
+
+    try {
+      // Llamada a la API para verificar el token
+      await axios.get('/api3/protected', {
+        headers: {
+          Authorization: `${token}`,
+        },
+      });
+      setIsAuthenticated(true); // El token es válido
+    } catch (error) {
+      console.error("Token inválido o expirado:", error);
+      logout(); // Si el token no es válido, cierra la sesión
+    }
+  };
 
   const login = (token: string) => {
     localStorage.setItem("authToken", token);
@@ -20,6 +45,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     localStorage.removeItem("authToken");
     setIsAuthenticated(false);
   };
+
+  // Verificar el token al cargar la aplicación
+  useEffect(() => {
+    verifyToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
